3af73388ed218b05828660eceaeec0206a97c23f
aistestonciute <72549610+aistestonciute@users.noreply.github.com>
2022-10-22 20:15:18 +0300

    Update review.js

--- data/review.js
+++ data/review.js
@@ -1,57 +1,129 @@
-module.exports = [
-    {
-        _id: 'review:1',
-        rating: 5,
-        date: '2022-10-10',
-        reviewText: 'Great book',
-        user: {
-            _id: 'user:1',
-            username: 'username1',
-            email: 'username1@email.com',
-            password: 'password'
-        },
-        book: '6'
+r1 = {
+    "_id" : 1,
+    "rating" : 4,
+    "date" : "2022-10-10",
+    "reviewText" : "Great book!",
+    "user" : {
+        "username" : "username1",
+        "email" : "username1@email.com",
+        "password" : "password123"
     },
+    "book" : 1
+}
 
-    {
-        _id: 'review:2',
-        rating: 3,
-        date: '2022-10-12',
-        reviewText: 'Good book',
-        user: {
-            _id: 'user:2',
-            username: 'username2',
-            email: 'username2@email.com',
-            password: 'password123'
-        },
-        book: '10'
+r2 = {
+    "_id" : 2,
+    "rating" : 5,
+    "date" : "2022-10-10",
+    "reviewText" : "Amazing book!",
+    "user" : {
+        "username" : "username2",
+        "email" : "username2@email.com",
+        "password" : "password12345"
     },
+    "book" : 2
+}
 
-    {
-        _id: 'review:3',
-        rating: 3,
-        date: '2022-10-10',
-        reviewText: 'Amazing book',
-        user: {
-            _id: 'user:1',
-            username: 'username1',
-            email: 'username1@email.com',
-            password: 'password'
-        },
-        book: '6'
+r3 = {
+    "_id" : 3,
+    "rating" : 2,
+    "date" : "2022-10-12",
+    "reviewText" : "Boring book!",
+    "user" : {
+        "username" : "username1",
+        "email" : "username1@email.com",
+        "password" : "password123"
     },
+    "book" : 2
+}
 
-    {
-        _id: 'review:4',
-        rating: 5,
-        date: '2022-10-15',
-        reviewText: 'Great book',
-        user: {
-            _id: 'user:3',
-            username: 'username3',
-            email: 'username3@email.com',
-            password: 'password'
-        },
-        book: '6'
-    }
-];
+r4 = {
+    "_id" : 4,
+    "rating" : 4,
+    "date" : "2022-10-10",
+    "reviewText" : "Awesome book!",
+    "user" : {
+        "username" : "username3",
+        "email" : "username3@email.com",
+        "password" : "password123456789"
+    },
+    "book" : 1
+}
+
+r5 = {
+    "_id" : 5,
+    "rating" : 4,
+    "date" : "2022-10-10",
+    "reviewText" : "Cool book!",
+    "user" : {
+        "username" : "username1",
+        "email" : "username1@email.com",
+        "password" : "password123"
+    },
+    "book" : 3
+}
+
+r6 = {
+    "_id" : 6,
+    "rating" : 4,
+    "date" : "2022-10-15",
+    "reviewText" : "Interesting book!",
+    "user" : {
+        "username" : "username1",
+        "email" : "username1@email.com",
+        "password" : "password123"
+    },
+    "book" : 6
+}
+
+r7 = {
+    "_id" : 7,
+    "rating" : 5,
+    "date" : "2022-10-22",
+    "reviewText" : "Masterpiece!",
+    "user" : {
+        "username" : "username1",
+        "email" : "username1@email.com",
+        "password" : "password123"
+    },
+    "book" : 5
+}
+
+r8 = {
+    "_id" : 8,
+    "rating" : 2,
+    "date" : "2022-10-21",
+    "reviewText" : "Bad book!",
+    "user" : {
+        "username" : "username4",
+        "email" : "username4@email.com",
+        "password" : "password123"
+    },
+    "book" : 1
+}
+
+r9 = {
+    "_id" : 9,
+    "rating" : 4,
+    "date" : "2022-10-10",
+    "reviewText" : "Cool book!",
+    "user" : {
+        "username" : "username4",
+        "email" : "username4@email.com",
+        "password" : "password123"
+    },
+    "book" : 4
+}
+
+r10 = {
+    "_id" : 10,
+    "rating" : 4,
+    "date" : "2022-10-12",
+    "reviewText" : "Cool book!",
+    "user" : {
+        "username" : "username5",
+        "email" : "username5@email.com",
+        "password" : "password555"
+    },
+    "book" : 3
+}
